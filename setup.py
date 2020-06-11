28ab0d2174bac2443ee27cb2353a7f684249f632
Arnau Pujol maryam <arnaupv@gmail.com>
2020-06-11 13:39:46 +0200

    requrements for installation added

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as open_file:
+    install_requires = open_file.read()
+
 setuptools.setup(
     name="nnsc",
     version="0.0.1",
@@ -12,6 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/arnaupujol/nnsc",
+    install_requires=install_requires,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2",
